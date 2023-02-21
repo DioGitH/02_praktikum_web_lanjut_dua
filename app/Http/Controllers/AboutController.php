c38e4b61e9698e2637fdd37e3e6557d27d9c86b9
= <=>
2023-02-21 16:46:27 +0700

    Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        echo "NIM: 2141720041";
+        echo "Nama: Maulidio Farhan Rizkullah";
+    }
+}
